6d9afa2f311ffca9431d72c03e83ef65cadb18ae
XuanSown <tranxuanson9423@gmail.com>
2026-07-10 09:47:32 +0700

    fix(admin): sidebar layout flex

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -197,6 +197,7 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
       {!isLoginPage && (
         <aside
         className={`
+          flex flex-col
           fixed top-0 left-0 z-50 h-full w-64 bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-800
           transform transition-transform duration-300 ease-in-out
           lg:translate-x-0
